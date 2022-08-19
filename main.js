7f36c2bf3163006688816aaf48cd10b11b87cf87
Vlythr <vlythr@pm.me>
2022-08-19 18:01:34 -0300

    Created a random password generator function createPassword()

--- main.js
+++ main.js
@@ -18,3 +18,25 @@ const keys = {
       return keys.symbol[Math.floor(Math.random() * keys.symbol.length)];
     }
   ];
+  
+  function createPassword() {
+    const upper = document.getElementById("upperCase").checked;
+    const lower = document.getElementById("lowerCase").checked;
+    const number = document.getElementById("number").checked;
+    const symbol = document.getElementById("symbol").checked;
+    if (upper + lower + number + symbol === 0) {
+      alert("Please check atleast one box!");
+      return;
+    }
+    const passwordBox = document.getElementById("passwordBox");
+    const length = document.getElementById("length");
+    let password = "";
+    while (length.value > password.length) {
+      let keyToAdd = getKey[Math.floor(Math.random() * getKey.length)];
+      let isChecked = document.getElementById(keyToAdd.name).checked;
+      if (isChecked) {
+        password += keyToAdd();
+      }
+    }
+    passwordBox.innerHTML = password;
+  }
